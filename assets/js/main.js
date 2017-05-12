824e1eac10d7fbe37af60fe3ff5a5bce6b763b30
berenisser <berenisser@gmail.com>
2017-05-11 21:58:17 -0300

    Todo el codigo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
-document.getElementById("toggle").addEventListener("click", function(event){event.preventDefault();
+document.getElementById("toggle").addEventListener("click", function(event){
+	event.preventDefault();
 	document.getElementById("header").classList.toggle("open");
 	document.getElementById("body").classList.toggle("overflow-hidden");
 });
@@ -7,9 +8,17 @@ var lastScrollTop = 0;
 window.addEventListener("scroll", function(){
 	var currentScroll = window.pageYOffSet || document.body.lastScrollTop; 
 	if(currentScroll>lastScrollTop){
-		console.log("down");
+		document.getElementById("header").classList.remove("header-in");
+		document.getElementById("header").style.opacity=0;
 	}else{
-		console.log("up");
+		document.getElementById("header").classList.add("header-in");
+		document.getElementById("header").classList.add("solid");
+		document.getElementById("header").style.opacity = 1;
 	}
+	if (currentScroll<=3) {
+			document.getElementById("header").classList.remove("solid");
+			document.getElementById("header").classList.remove("header-in");
+	}
+
 	lastScrollTop = currentScroll;
-});
+},false);
