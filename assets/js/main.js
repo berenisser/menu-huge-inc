0a5adaefcd8f26b0837250e4f3a05ea5a6591013
berenisser <berenisser@gmail.com>
2017-05-11 21:42:53 -0300

    sigo intentando hacer funcionar la burger

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,15 @@
-document.getElementById('toggle').addEventListener("click", function(event){event.preventDefault();
-	document.getElementById("nav-header").classList.toggle("open");
+document.getElementById("toggle").addEventListener("click", function(event){event.preventDefault();
+	document.getElementById("header").classList.toggle("open");
 	document.getElementById("body").classList.toggle("overflow-hidden");
-})
+});
+
+var lastScrollTop = 0;
+window.addEventListener("scroll", function(){
+	var currentScroll = window.pageYOffSet || document.body.lastScrollTop; 
+	if(currentScroll>lastScrollTop){
+		console.log("down");
+	}else{
+		console.log("up");
+	}
+	lastScrollTop = currentScroll;
+});
